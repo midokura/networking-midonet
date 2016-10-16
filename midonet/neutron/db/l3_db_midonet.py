4a44d25e79230bcdb30357e437e2fb66e3fb2fc9
Alex Bikfalvi <alex.bikfalvi@midokura.com>
2016-10-16 23:36:44 +0200

    FIP6: Support creating IPv6 FIP
    This modifies l3_db_midonet to override the _create_floatingip
    method in order to allow the creation of floating IPs with IPv6
    addresses. The patch also fixes a bug in the overriden method
    where if creating the floating IP will fail after creating the
    port, the port was not deleted.
    
    Ref: MI-1603
    
    Change-Id: Ia852002aaad32e5c91c45ecf5f93a8c0c432c1b7
    Signed-off-by: Alex Bikfalvi <alex.bikfalvi@midokura.com>

--- midonet/neutron/db/l3_db_midonet.py
+++ midonet/neutron/db/l3_db_midonet.py
@@ -14,13 +14,22 @@
 #    under the License.
 
 from oslo_utils import excutils
+from oslo_utils import uuidutils
+
+from neutron_lib import constants as n_const
+from neutron_lib import exceptions as n_exc
 
 from neutron.callbacks import events
 from neutron.callbacks import exceptions
 from neutron.callbacks import registry
 from neutron.callbacks import resources
+from neutron.common import utils
+from neutron.db import l3_db
 from neutron.db import l3_gwmode_db
 from neutron.extensions import l3
+from neutron.plugins.common import utils as p_utils
+
+DEVICE_OWNER_FLOATINGIP = n_const.DEVICE_OWNER_FLOATINGIP
 
 
 class MidonetL3DBMixin(l3_gwmode_db.L3_NAT_db_mixin):
@@ -37,3 +46,92 @@ class MidonetL3DBMixin(l3_gwmode_db.L3_NAT_db_mixin):
                 if len(e.errors) == 1:
                     raise e.errors[0].error
                 raise l3.RouterInUse(router_id=router_id, reason=e)
+
+    def _port_ipv6_fixed_ips(self, port):
+        return [ip for ip in port['fixed_ips']
+                if netaddr.IPAddress(ip['ip_address']).version == 6]
+
+    # REVISIT(bikfalvi): This method is a copy of the base class method,
+    # to allow IPv6 by substituting the calls to _is_ipv4_network and
+    # _port_ipv4_fixed_ips by equivalent code that allow both IPv6 and IPv4 FIPs.
+    # Additionally, a call to delete_port has been added in case the port cannot
+    # be created.
+    def _create_floatingip(self, context, floatingip,
+                           initial_status=n_const.FLOATINGIP_STATUS_ACTIVE):
+        fip = floatingip['floatingip']
+        fip_id = uuidutils.generate_uuid()
+
+        f_net_id = fip['floating_network_id']
+        if not self._core_plugin._network_is_external(context, f_net_id):
+            msg = _("Network %s is not a valid external network") % f_net_id
+            raise n_exc.BadRequest(resource='floatingip', msg=msg)
+
+        if not any(self._core_plugin._get_network(context, f_net_id).subnets):
+            msg = _("Network %s does not contain any subnet") % f_net_id
+            raise n_exc.BadRequest(resource='floatingip', msg=msg)
+
+        dns_integration = utils.is_extension_supported(self._core_plugin,
+                                                       'dns-integration')
+        with context.session.begin(subtransactions=True):
+            # This external port is never exposed to the tenant.
+            # it is used purely for internal system and admin use when
+            # managing floating IPs.
+
+            port = {'tenant_id': '',  # tenant intentionally not set
+                    'network_id': f_net_id,
+                    'admin_state_up': True,
+                    'device_id': fip_id,
+                    'device_owner': DEVICE_OWNER_FLOATINGIP,
+                    'status': n_const.PORT_STATUS_NOTAPPLICABLE,
+                    'name': ''}
+            if fip.get('floating_ip_address'):
+                port['fixed_ips'] = [
+                    {'ip_address': fip['floating_ip_address']}]
+
+            if fip.get('subnet_id'):
+                port['fixed_ips'] = [
+                    {'subnet_id': fip['subnet_id']}]
+
+            # 'status' in port dict could not be updated by default, use
+            # check_allow_post to stop the verification of system
+            external_port = p_utils.create_port(self._core_plugin,
+                                                context.elevated(),
+                                                {'port': port},
+                                                check_allow_post=False)
+            # Ensure IP addresses are allocated on external port
+            # preferring IPv4 over IPv6 ones
+            external_ips = self._port_ipv4_fixed_ips(external_port)
+            if not external_ips:
+                external_ips = self._port_ipv6_fixed_ips(external_port)
+                if not external_ips:
+                    self._core_plugin.delete_port(context, external_port['id'])
+                    raise n_exc.ExternalIpAddressExhausted(net_id=f_net_id)
+
+            floating_fixed_ip = external_ips[0]
+            floating_ip_address = floating_fixed_ip['ip_address']
+            floatingip_db = l3_db.FloatingIP(
+                id=fip_id,
+                tenant_id=fip['tenant_id'],
+                status=initial_status,
+                floating_network_id=fip['floating_network_id'],
+                floating_ip_address=floating_ip_address,
+                floating_port_id=external_port['id'],
+                description=fip.get('description'))
+            # Update association with internal port
+            # and define external IP address
+            self._update_fip_assoc(context, fip,
+                                   floatingip_db, external_port)
+            context.session.add(floatingip_db)
+            floatingip_dict = self._make_floatingip_dict(
+                floatingip_db, process_extensions=False)
+            if dns_integration:
+                dns_data = self._process_dns_floatingip_create_precommit(
+                    context, floatingip_dict, fip)
+
+        if dns_integration:
+            self._process_dns_floatingip_create_postcommit(context,
+                                                           floatingip_dict,
+                                                           dns_data)
+        self._apply_dict_extend_functions(l3.FLOATINGIPS, floatingip_dict,
+                                          floatingip_db)
+        return floatingip_dict
